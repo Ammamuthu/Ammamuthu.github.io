78365965059e4c1325c80a4e6e7956cc3d12da74
Ammamuthu <130463974+Ammamuthu@users.noreply.github.com>
2024-01-19 16:53:00 +0530

    Add files via upload
    First push on folio

---
+++ script.js
@@ -0,0 +1,22 @@
+// hover content in exeperience tag
+
+let tagone = document.querySelector('.one')
+let tagtwo =  document.querySelector('.two')
+
+
+console.log("testing")
+
+function Mover1(obj){
+    obj.innerHTML = `<p style ="text-align : center ; padding :10px;">Working in a Ebs apps dba side , like upgradation , migration , backupand Build disaster recovery</p>`
+}
+function Mout1(obj){
+    obj.innerHTML = `<h3> Oracle Dba </h3> <i class="fa-solid fa-database" style = "font-size: 60px;margin-top:60px;"></i>`
+}
+
+function Mover2(obj){
+    obj.innerHTML = `<h3>Time Tracker</h3> <i class="fa-solid fa-user-clock" style = "font-size: 60px;margin-top:60px;"></i>`
+}
+
+function Mout2(obj){
+    obj.innerHTML = `<p style ="text-align : center ; padding :10px;">Develop a Internal product called Time tracking system using Django , Python , Html ,Css Bootstrap and DB PostgreSQL</p>`
+}
